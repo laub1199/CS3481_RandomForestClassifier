396b48869f981eabe339a6d2b679d96cba7a5865
Sennett Lau <laub1199@gmail.com>
2021-03-30 01:37:37 +0800

    show most accurate forest tree 1

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn import tree
 import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
+import graphviz
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -17,18 +19,42 @@ if __name__ == '__main__':
     x = df.drop('class', axis=1)
     y = df['class']
 
-    train_data, text_data, train_label, test_label = train_test_split(x, y, test_size=0.2, random_state=6)
+    train_data, test_data, train_label, test_label = train_test_split(x, y, test_size=0.2)
 
-    list = []
+    train_list = []
+    test_list = []
+    clfs = []
     for i in range(1,20):
         clf = RandomForestClassifier(n_estimators=i)
         clf = clf.fit(train_data, train_label)
-        prediction = clf.predict(text_data)
-        accuracy = accuracy_score(test_label, prediction)
-        print(str(i) + ': ' + str(accuracy))
-        obj = [i, accuracy]
-        list.append(obj)
 
-    list = sorted(list, key=lambda x:x[0], reverse=True)
+        # train_prediction = clf.predict(train_data)
+        # train_accuracy = accuracy_score(train_label, train_prediction)
 
-    print(list)
+        test_prediction = clf.predict(test_data)
+        clfs.append(clf)
+        test_accuracy = accuracy_score(test_label, test_prediction)
+
+        # print(str(i) + ': ' + str(train_accuracy) + str(test_accuracy))
+        print(str(i) + ': ' + str(test_accuracy))
+
+        # obj = [i, train_accuracy]
+        # train_list.append(obj)
+
+        obj = [i, test_accuracy]
+        test_list.append(obj)
+
+    # train_list = sorted(train_list, key=lambda x:x[1], reverse=True)
+    test_list = sorted(test_list, key=lambda x:x[1], reverse=True)
+
+    # print(train_list)
+    print(test_list)
+
+    dot_data = tree.export_graphviz(clfs[test_list[0][0]].estimators_[1], out_file=None, filled=True,
+                                    feature_names=["pelvic_incidence", "pelvic_tilt", "lumbar_lordosis_angle",
+                                                   "sacral_slope", "pelvic_radius", "degree_spondylolisthesis"],
+                                    class_names=["Hernia", "Normal", "Spondylolisthesis"], rounded=True,
+                                    special_characters=True)
+
+    graph_training = graphviz.Source(dot_data)
+    graph_training.render('Graph', view=True)
